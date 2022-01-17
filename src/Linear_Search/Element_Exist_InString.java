55c27422cc47883ad1434a1a8631dec8c71f590f
Yash Gupta <yash8917gupta@gmail.com>
2022-01-17 22:57:08 +0530

    INITIAL Commit

--- src/Linear_Search/Element_Exist_InString.java
+++ src/Linear_Search/Element_Exist_InString.java
@@ -1,5 +1,23 @@
 package Linear_Search;
 
 public class Element_Exist_InString {
+    static boolean linearSearch(String str, char target) {
+        if (str.length() == 0) {
+            return false;
+        }
+        for (int index = 0; index < str.length(); index++) {
+            if (target == str.charAt(index)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
+    public static void main(String[] args) {
+        String str="Welcome";
+        char target='o';
+        System.out.println(linearSearch(str,target));
+
+
+    }
 }
