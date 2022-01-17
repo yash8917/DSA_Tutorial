117f5d4048d942a2532a09046e449714ef3eb3dc
Yash Gupta <yash8917gupta@gmail.com>
2022-01-17 23:21:51 +0530

    INITIAL Commit

---
+++ src/Linear_Search/Search_InArray_Limt.java
@@ -0,0 +1,22 @@
+package Linear_Search;
+
+public class Search_InArray_Limt {
+     static public int Search(int[] arr,int target,int start,int end){
+        if (arr.length==0){
+            return -1;
+        }
+        for (int index = start; index <=end ; index++) {
+            if (target==arr[index]){
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={12,3,34,45,56,3,32,57,65};
+        int target=56;
+        int res=Search(arr,target,2,6);
+        System.out.println(res);
+    }
+}
