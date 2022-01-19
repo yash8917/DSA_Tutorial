21cc2838e11b51335b3e3d45580adc3397fe1671
Yash Gupta <yash8917gupta@gmail.com>
2022-01-19 20:26:51 +0530

    INITIAL Commit

--- src/Input_5_ElementInArray.java
+++ src/Input_5_ElementInArray.java
@@ -13,7 +13,7 @@ public class Input_5_ElementInArray {
 
     public static void main(String[] args) {
         Input_5_ElementInArray obj=new Input_5_ElementInArray();
-        System.out.println("Elements are :-");
+        System.out.println("Elements are :- ");
         for (int i = 0; i <obj.arr.length ; i++) {
             System.out.println(obj.arr[i]);
         }
