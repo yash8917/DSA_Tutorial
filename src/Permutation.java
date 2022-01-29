e1afa6e2c828f3eadda2345423ced96341510f93
Yash Gupta <yash8917gupta@gmail.com>
2022-01-29 19:17:53 +0530

    INITIAL Commit

---
+++ src/Permutation.java
@@ -0,0 +1,32 @@
+// permutation example
+// the input string is "abc"
+// print all the permutations of the given string
+// ans will be :-
+// abc
+// acb
+// bac
+// bca
+// cab
+// cba
+    public class Permutation {
+        public static void ans(String inp, String ans)
+        {
+            // base condition
+
+            if(inp.length() ==0 )
+            {
+                System.out.println(ans);
+                return;
+            }
+            for(int i =0;i<inp.length();i++)
+            {
+                String remainingString= inp.substring(0,i)+ inp.substring(i+1) ;
+                char extract = inp.charAt(i);
+                ans(remainingString, ans +extract);
+            }
+        }
+        public static void main(String[] args) {
+            ans("abc"," ");
+        }
+}
+
