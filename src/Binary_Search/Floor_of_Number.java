78e4bc6dec6dca8e8340df1efc65a2d00028a6d4
Yash Gupta <yash8917gupta@gmail.com>
2022-01-31 10:08:49 +0530

    INITIAL Commit

---
+++ src/Binary_Search/Floor_of_Number.java
@@ -0,0 +1,33 @@
+package Binary_Search;
+
+public class Floor_of_Number {
+    public static void main(String[] args) {
+        int[] arr={2,3,5,9,14,16,18};
+        int target=15;
+        int ans=floor(arr,target);
+        System.out.println("The Floor of a no: "+ans);
+    }
+    static int floor(int[] arr , int target){
+        int start=0;
+        int end = arr.length -1;
+        //Middle of the array
+//        int mid=(start+end) / 2; (This my possible exeed the Range of the integer
+        while (start <= end){
+            //        int mid=(start+end) / 2; (This my possible exeed the Range of the integer
+            int mid=start + (end - start) / 2;
+            //If the target Element is greater then the biggest no of array
+            if (target > arr[arr.length-1]){
+                return -1;
+            }
+            if (target < arr[mid]){
+                end = mid - 1;
+            }
+            else if (target >arr[mid]){
+                start = mid + 1;
+            }else {
+                return mid;
+            }
+        }
+        return end;
+    }
+}
