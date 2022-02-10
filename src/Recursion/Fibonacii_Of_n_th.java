5768b46e1e2899408a035359cf134fc64ed7625f
Yash Gupta <yash8917gupta@gmail.com>
2022-02-10 23:06:31 +0530

    INITIAL Commit

---
+++ src/Recursion/Fibonacii_Of_n_th.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class Fibonacii_Of_n_th {
+    public static void main(String[] args) {
+        int ans=fibo(4);
+        System.out.println("The Fibonacii no of n^th term is: "+ans);
+    }
+    static int fibo(int n){
+        //Base condition
+        if(n< 2){
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
