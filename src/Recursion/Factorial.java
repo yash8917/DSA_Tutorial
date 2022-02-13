b60fad0bef045339b44a64a6a9375c8612651ce8
Yash Gupta <yash8917gupta@gmail.com>
2022-02-13 18:32:23 +0530

    INITIAL Commit

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,30 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static int fact(int n){
+        //Base case-This is called Head Recursion
+        if( n== 1 ){
+            return 1;
+        }
+        //recursive call with smaller value
+        int recAns=fact(n-1);
+        int myAns=n*recAns;
+        return myAns;
+    }
+//    //2nd method Approach is the Tail Recursion
+//    public static void fact(int n, int ans){
+//        if(n==0 || n==1){
+//            System.out.println(ans);
+//            return;
+//        }
+//        fact(n-1, ans*n);
+//  }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter te number:");
+        int n=sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
