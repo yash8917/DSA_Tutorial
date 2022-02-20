fe3add3ff71012925abc0311852a0c537895eb02
Yash Gupta <yash8917gupta@gmail.com>
2022-02-20 11:25:51 +0530

    INITIAL Commit

--- src/Linked_List/IO_Doubly_Getter_Setter.java
+++ src/Linked_List/IO_Doubly_Getter_Setter.java
@@ -1,4 +1,7 @@
 package Linked_List;
+
+import java.util.Scanner;
+
 class Node4 {
     int data;
     Node4 next; //Self Refrential variable
@@ -16,15 +19,19 @@ public class IO_Doubly_Getter_Setter {
     display(head);
     }
     public static Node4 createLinkedList(){
+        Scanner sc =new Scanner(System.in);
+
         Node4 newnode1=new Node4();
         Node4 newnode2=new Node4();
+        newnode1.setData(200);
+        newnode2.setData(400);
         newnode1.next=newnode2;
         newnode2.prev=newnode1;
         return newnode1;
     }
     public static void display(Node4 temp){
-        System.out.println(temp.data);
-        System.out.println(temp.next.data);
+        System.out.println(temp.getData());
+        System.out.println(temp.next.getData());
         System.out.println(temp.prev);
         System.out.println(temp.next.prev);
     }
