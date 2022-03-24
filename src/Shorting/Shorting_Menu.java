895c30cd7084e4aeb579b227d17a61601c8001da
Yash Gupta <yash8917gupta@gmail.com>
2022-03-24 23:21:58 +0530

    INITIAL Commit

--- src/Shorting/Shorting_Menu.java
+++ src/Shorting/Shorting_Menu.java
@@ -1,32 +1,39 @@
 package Shorting;
 
 import java.beans.Expression;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Shorting_Menu {
-    static int[] arr;
-    Shorting_Menu(){
-        int n;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the size of the Array:-> ");
-        n= sc.nextInt();
-        for (int i = 0; i < n ; i++) {
-            arr[i]= sc.nextInt();
+    public static void main(String[] args) {
+        int[] arr={17,23,11,10,3,9};
+//       bubbleSort(arr);
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i <arr.length-1 ; i++) {
+            int min =i;
+            for (int j = i+1; j <arr.length ; j++) {
+                if(arr[min] > arr[j]){
+                    min=j;
+                }
+                int temp=arr[i];
+                arr[i]=arr[min];
+                arr[min]=temp;
+            }
         }
     }
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j] < arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                }
 
-    public static void main(String[] args) {
-        Shorting_Menu obj=new Shorting_Menu();
-        Scanner sc=new Scanner(System.in);
-        int num= sc.nextInt();
-        switch (num){
-            case 1:
-                System.out.println(arr[1]);
-            case 2:
-                System.out.println(arr[2]);
-            default:
-                System.out.println("Index is not Found");
-
+            }
         }
     }
 }
