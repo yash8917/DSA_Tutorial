742e4f30cb0edcdf9f97e3398a82a777a874a0b6
Yash Gupta <yash8917gupta@gmail.com>
2022-04-04 22:50:27 +0530

    INITIAL Commit

---
+++ src/Binary_Tree/Traversal_PreOrder.java
@@ -0,0 +1,35 @@
+package Binary_Tree;
+class BinaryNode3{
+    int data;
+    BinaryNode3 left;
+    BinaryNode3 right;
+    public BinaryNode3(int  data){
+        this.data=data;
+    }
+}
+public class Traversal_PreOrder {
+    public static void main(String[] args) {
+        // Creating the Object
+        BinaryNode3 Root =new BinaryNode3(10);
+        BinaryNode3 RootLeft=new BinaryNode3(5);
+        BinaryNode3 Rootright =new BinaryNode3(15);
+        Root.left=RootLeft;
+        Root.right=Rootright;
+        BinaryNode3 leftl=new BinaryNode3(3);
+        BinaryNode3 leftr=new BinaryNode3(8);
+
+        Root.left=leftl;
+        Root.right=leftr;
+
+        preOrder(Root);
+    }
+public static void preOrder(BinaryNode3 root){
+        if(root == null){
+            return;
+        }
+    System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+}
+
+}
