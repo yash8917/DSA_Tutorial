58860f157e79f3c101bfad607d0103b7fc18aa66
Yash Gupta <yash8917gupta@gmail.com>
2022-04-06 23:21:41 +0530

    INITIAL Commit

--- src/Linked_List/Reverse_DoublyLinkedList.java
+++ src/Linked_List/Reverse_DoublyLinkedList.java
@@ -1,32 +1,4 @@
 package Linked_List;
-//class Node3{
-//    int data;
-//    Node3 next;
-//
-//    public Node3(int data) {
-//        this.data = data;
-//    }
-//}
-//public class Reverse_LinkedList {
-//    public static void main(String[] args) {
-//        Node3 head=CreatLinkedList();
-//
-//    }
-//    public static Node3  CreatLinkedList() {
-//        Node3 n1 = new Node3(3);
-//        Node3 n2 = new Node3(4);
-//        Node3 n3 = new Node3(5);
-//        Node3 n4 = new Node3(6);
-//        n1.next = n2;
-//        n2.next = n3;
-//        n3.next = n4;
-//        return n1;
-//    }
-//    public static Node3 print(Node3 head){
-//
-//    }
-//}
-// Java program for reversing the linked list
 
 class LinkedList {
     static Node head;
