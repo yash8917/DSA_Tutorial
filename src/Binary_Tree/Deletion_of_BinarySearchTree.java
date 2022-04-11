88b744c51839d30e3dd3309e097c10b02bccd48f
Yash Gupta <yash8917gupta@gmail.com>
2022-04-11 23:39:34 +0530

    INITIAL Commit

--- src/Binary_Tree/Deletion_of_BinarySearchTree.java
+++ src/Binary_Tree/Deletion_of_BinarySearchTree.java
@@ -145,8 +145,7 @@ class BinarySearchTree {
 
         System.out.println("\nDelete 30");
         tree.deleteKey(30);
-        System.out.println(
-                "Inorder traversal of the modified tree");
+        System.out.println("Inorder traversal of the modified tree");
         tree.inorder();
 
         System.out.println("\nDelete 50");
