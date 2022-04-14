27c9c72eeec2acd7a5775b707ba89fdb151b01a6
Yash Gupta <yash8917gupta@gmail.com>
2022-04-14 17:59:03 +0530

    INITIAL Commit

--- src/Binary_Tree/Traversal_Methods.java
+++ src/Binary_Tree/Traversal_Methods.java
@@ -17,30 +17,36 @@ public class Traversal_Methods {
         Root.right=Rootright;
         BinaryNode3 leftl=new BinaryNode3(3);
         BinaryNode3 leftr=new BinaryNode3(8);
-
         Root.left=leftl;
         Root.right=leftr;
-
         preOrder(Root);
+        System.out.println();
+        inOrder(Root);
+        System.out.println();
+        postOrder(Root);
     }
 public static void preOrder(BinaryNode3 root){
+
         if(root == null){
             return;
         }
-    System.out.println(root.data);
+    System.out.print(root.data+" ");
         preOrder(root.left);
         preOrder(root.right);
 }
 public static void inOrder(BinaryNode3 root){
+
         if (root == null)
             return;
         inOrder(root.left);
-    System.out.println(root.data);
+    System.out.print(root.data+" ");
          inOrder(root.right);
 }
 public static void postOrder(BinaryNode3 root){
+    if (root == null)
+        return;
        postOrder(root.left);
        postOrder(root.right);
-    System.out.println(root.data);
+    System.out.print(root.data+" ");
 }
 }
